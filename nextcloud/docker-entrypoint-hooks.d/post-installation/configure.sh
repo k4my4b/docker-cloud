2f834873925e751b9a9ba720da81f29434482d6a
Kamyab Sherafat <k4my4b@gmail.com>
2023-09-19 14:27:30 +0100

    disable knowlegebase

--- nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
@@ -4,4 +4,5 @@
 php occ app:disable firstrunwizard
 
 # config 
+php occ config:system:set --type=boolean --value="$KNOWLEDGEBASE_ENABLED" -- knowledgebaseenabled 
 php occ config:system:set --type=string --value="$DEFAULT_PHONE_REGION" -- default_phone_region 
