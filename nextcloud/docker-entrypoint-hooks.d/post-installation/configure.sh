bfbc26d46e044887637ab8d8c1045986a3d5d30f
Kamyab Sherafat <k4my4b@gmail.com>
2023-09-19 14:52:31 +0100

    install spreed

--- nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
@@ -21,6 +21,7 @@ php occ app:install terms_of_service
 php occ app:install calendar 
 php occ app:install contacts
 php occ app:install mail
+php occ app:install spreed
 
 # config 
 php occ config:system:set --type=boolean --value="$DISABLE_WEB_UPGRADE" -- upgrade.disable-web 
