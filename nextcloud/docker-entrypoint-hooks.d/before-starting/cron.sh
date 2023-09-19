ee35234c4ad18f02732c234bd337e0cf55512d29
Kamyab Sherafat <k4my4b@gmail.com>
2023-09-20 00:21:49 +0100

    made cron.sh a before-starting hook and modified it to run in the background

---
+++ nextcloud/docker-entrypoint-hooks.d/before-starting/cron.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cron() {
+  if [[ -z "${CRON_INTERVAL}" ]]; then
+    echo "CRON_INTERVAL was not set, defaulting to 5 minutes"
+    SLEEP_DURATION=5m
+  elif ! [[ "${CRON_INTERVAL}" =~ ^[0-9]+[mshd]$ ]]; then
+    echo "CRON_INTERVAL value is not valid, defaulting to 5 minutes"
+    SLEEP_DURATION=5m
+  else
+    SLEEP_DURATION="${CRON_INTERVAL}"
+  fi
+
+  while true; do
+    if ! php -f /var/www/html/cron.php; then
+      echo "Something went wrong with cron.php, will try again in ${SLEEP_DURATION}"
+    fi
+    sleep "${SLEEP_DURATION}"
+  done
+}
+
+# start running cron in the background
+cron &
