b062480c83701d691692405ca43a413340e3184f
Kamyab Sherafat <k4my4b@gmail.com>
2023-09-19 14:35:44 +0100

    set force_language and force_locale from $FORCE_LANGUAGE and $FORCE_LOCALE respectively

--- nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
@@ -8,3 +8,5 @@ php occ config:system:set --type=boolean --value="$DISABLE_WEB_UPGRADE" -- upgra
 php occ config:system:set --type=boolean --value="$KNOWLEDGEBASE_ENABLED" -- knowledgebaseenabled 
 php occ config:system:set --type=string --value="$SKELETON_DIRECTORY" -- skeletondirectory
 php occ config:system:set --type=string --value="$DEFAULT_PHONE_REGION" -- default_phone_region 
+php occ config:system:set --type=string --value="$FORCE_LANGUAGE" -- force_language 
+php occ config:system:set --type=string --value="$FORCE_LOCALE" -- force_locale
