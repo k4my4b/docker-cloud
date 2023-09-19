0d94f284c576881d2ea34a4dc4effc8ff10ae989
Kamyab Sherafat <k4my4b@gmail.com>
2023-09-19 14:29:29 +0100

    disable web upgrade (upgrade.disable-web)

--- nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
@@ -4,5 +4,6 @@
 php occ app:disable firstrunwizard
 
 # config 
+php occ config:system:set --type=boolean --value="$DISABLE_WEB_UPGRADE" -- upgrade.disable-web 
 php occ config:system:set --type=boolean --value="$KNOWLEDGEBASE_ENABLED" -- knowledgebaseenabled 
 php occ config:system:set --type=string --value="$DEFAULT_PHONE_REGION" -- default_phone_region 
