6cb4cf76e3838940dd67cef4f334c4247c77b1d3
Kamyab Sherafat <k4my4b@gmail.com>
2023-10-04 22:51:56 +0100

    add imaginary
    the upstream nextcloud-aio version of imaginary container will suffice for now, but will move to a custom one down the line

--- nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/configure.sh
@@ -20,21 +20,23 @@ php occ app:install groupfolders
 php occ app:install deck
 php occ app:install forms
 php occ app:install terms_of_service
-php occ app:install calendar 
+php occ app:install calendar
 php occ app:install contacts
 php occ app:install mail
 php occ app:install spreed
 php occ app:install quota_warning
 php occ app:install files_antivirus
 
-# config 
-php occ config:system:set --type=boolean --value="$DISABLE_WEB_UPGRADE" -- upgrade.disable-web 
-php occ config:system:set --type=boolean --value="$KNOWLEDGEBASE_ENABLED" -- knowledgebaseenabled 
+# config
+php occ config:system:set --type=boolean --value="$DISABLE_WEB_UPGRADE" -- upgrade.disable-web
+php occ config:system:set --type=boolean --value="$KNOWLEDGEBASE_ENABLED" -- knowledgebaseenabled
 php occ config:system:set --type=string --value="$SKELETON_DIRECTORY" -- skeletondirectory
-php occ config:system:set --type=string --value="$DEFAULT_PHONE_REGION" -- default_phone_region 
-php occ config:system:set --type=string --value="$DEFAULT_LANGUAGE" -- default_language 
+php occ config:system:set --type=string --value="$DEFAULT_PHONE_REGION" -- default_phone_region
+php occ config:system:set --type=string --value="$DEFAULT_LANGUAGE" -- default_language
 php occ config:system:set --type=string --value="$DEFAULT_LOCALE" -- default_locale
-php occ config:system:set --type=string --value="$THEME" -- theme 
+php occ config:system:set --type=string --value="$THEME" -- theme
+php occ config:system:set --value="OC\\Preview\\Imaginary" -- enabledPreviewProviders 0
+php occ config:system:set --value="http://$IMAGINARY_HOST:$IMAGINARY_PORT" -- preview_imaginary_url
 php occ twofactorauth:enforce "$([ "$ENFORCE_TWOFACTORAUTH" = true ] && echo '--on' || echo '--off')"
 php occ config:app:set --value="$([ "$ENABLE_TALK_CHANGELOG" = true ] && echo 'yes' || echo 'no')" -- spreed changelog
 php occ config:app:set --value="$([ "$HARDENED_PASSWORD_POLICY" = true ] && echo 1 || echo 0)" -- password_policy enforceUpperLowerCase
@@ -43,8 +45,8 @@ php occ config:app:set --value="$([ "$HARDENED_PASSWORD_POLICY" = true ] && echo
 php occ config:app:set --value="$([ "$HARDENED_PASSWORD_POLICY" = true ] && echo 10 || echo 0)" -- password_policy maximumLoginAttempts
 php occ config:app:set --value="$([ "$ENABLE_NOTIFICATION_SOUND" = true ] && echo 'yes' || echo 'no')" -- notifications sound_notification
 php occ config:app:set --value="$([ "$ENABLE_NOTIFICATION_SOUND" = true ] && echo 'yes' || echo 'no')" -- notifications sound_talk
-php occ config:app:set --value="daemon" -- files_antivirus av_mode 
-php occ config:app:set --value="$AV_HOST" -- files_antivirus av_host 
-php occ config:app:set --value="$AV_PORT" -- files_antivirus av_port 
-php occ config:app:set --value="$AV_STREAM_MAX_LENGTH" -- files_antivirus av_stream_max_length 
-php occ config:app:set --value="$AV_MAX_FILE_SIZE" -- files_antivirus av_max_file_size 
+php occ config:app:set --value="daemon" -- files_antivirus av_mode
+php occ config:app:set --value="$AV_HOST" -- files_antivirus av_host
+php occ config:app:set --value="$AV_PORT" -- files_antivirus av_port
+php occ config:app:set --value="$AV_STREAM_MAX_LENGTH" -- files_antivirus av_stream_max_length
+php occ config:app:set --value="$AV_MAX_FILE_SIZE" -- files_antivirus av_max_file_size
