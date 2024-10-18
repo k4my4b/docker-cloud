1e5127cea79ff37b8cac937f8758d8595b8a2d09
Kamyab Sherafat <k4my4b@gmail.com>
2024-10-18 01:59:16 +0100

    run nginx (even master) as the nginx user
    make generated ssl certificate accessible to nginx user

--- nginx/init.d/start/01-gen-ssl.sh
+++ nginx/init.d/start/01-gen-ssl.sh
@@ -16,6 +16,8 @@ fi
 
 # Generate self-signed SSL certificates
 echo "Generating (self-signed) SSL certificates"
-exec openssl ecparam -name prime256v1 -genkey -noout -out "$SSL_DIR/nginx.key" && \
+openssl ecparam -name prime256v1 -genkey -noout -out "$SSL_DIR/nginx.key" && \
     openssl req -x509 -new -key "$SSL_DIR/nginx.key" -out "$SSL_DIR/nginx.crt" \
     -days 36500 -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=localhost"
+
+chown 100:101 "$SSL_DIR"/nginx.crt "$SSL_DIR"/nginx.key
