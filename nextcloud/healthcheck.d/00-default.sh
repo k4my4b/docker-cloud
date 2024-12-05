5fb3f6ed1376c90dbd89604fbcc83069b30b963a
Kamyab Sherafat <k4my4b@gmail.com>
2024-12-05 03:07:22 +0000

    feat: replace nginx and php-fpm with NGINX Unit
    - Removed `nginx` service from `docker-compose.yml`, as NGINX Unit will handle the web server tasks.
    - Replaced `php-fpm` with NGINX Unit in Nextcloud Docker setup, consolidating PHP and web serving in a single service.
    - Added a multistage Dockerfile build for NGINX Unit with njs and PHP 8.2 support.
    - Added configuration for NGINX Unit with Nextcloud routing rules (`nextcloud.json`).
    - Updated health check script to validate NGINX Unit and Nextcloud service.
    - Removed legacy `php-fpm` configuration files.
    
    This change improves performance, simplifies the architecture and reduces the number of running services by using NGINX Unit for both PHP execution and serving static content.

--- nextcloud/healthcheck.d/00-default.sh
+++ nextcloud/healthcheck.d/00-default.sh
@@ -1,9 +1,24 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 nc -z 127.0.0.1 9000 || {
-    echo "Can't reach nextcloud on local port 9000"
+    echo "Unit is not listening on local port 9000"
     exit 1
 }
 
-echo "Nextcloud is healthy"
+curl -sf localhost:9000 > /dev/null || {
+    echo "Failed to get a status response from unit"
+    exit 1
+}
+
+test "$(occ status --no-warnings --output=json | jq -r '.installed')" = "false" && {
+    echo "Nextcloud is not installed"
+    exit 0
+}
+
+curl -fs localhost > /dev/null || {
+    echo "Failed to reach nextcloud on localhost"
+    exit 1
+}
+
+echo "Instance is healthy"
 exit 0
--- nextcloud/init.d/start/04-start-nextcloud.sh
+++ nextcloud/init.d/start/04-start-nextcloud.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
-echo "Starting nextcloud"
-# Start php-fpm82 in the background
-php-fpm82 --nodaemonize --fpm-config /etc/php82/php-fpm.d/nextcloud.conf &
+echo "Starting unitd"
+# Start unitd in the background
+unitd --no-daemon --user unit --group www-data --control 0.0.0.0:9000 &
 
-# Capture the PID of the last command executed (php-fpm)
-PHP_FPM_PID=$!
+# Capture the PID of the last command executed (unitd)
+UNITD_PID=$!
 
-# Wait for php-fpm to start
+# Wait for unitd to start
 sleep 2
 
+UNITD_CONFIG="etc/unit/nextcloud.json"
 # Check if the process directory exists in /proc
-test -d /proc/$PHP_FPM_PID && {
-    echo "nextcloud started successfully"
-    exit 0
+test -d /proc/$UNITD_PID && {
+    echo "unitd started successfully"
+    curl -s -X PUT --data-binary "@$UNITD_CONFIG" localhost:9000/config/ && {
+        echo "Nextcloud started successfully"
+        exit 0
+    }
+    echo "Failed to start nextcloud"
+    exit 1
 }
 
-echo "Failed to start nextcloud"
+echo "Failed to start unitd"
 exit 1
