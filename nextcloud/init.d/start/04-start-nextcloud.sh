0b57c21a2ccce3a9e34afcb3a01663cdc091878a
Kamyab Sherafat <k4my4b@gmail.com>
2024-12-16 01:24:01 +0000

    fix: verification of Nextcloud configuration in startup script
    - Properly verify the success of the PUT request to localhost:9000/config/ using jq
    - Ensure the response is correctly checked and logged for accurate error reporting

--- nextcloud/init.d/start/04-start-nextcloud.sh
+++ nextcloud/init.d/start/04-start-nextcloud.sh
@@ -14,10 +14,12 @@ UNITD_CONFIG="etc/unit/nextcloud.json"
 # Check if the process directory exists in /proc
 test -d /proc/$UNITD_PID && {
     echo "unitd started successfully"
-    curl -s -X PUT --data-binary "@$UNITD_CONFIG" localhost:9000/config/ && {
+    RESPONSE="$(curl -sX PUT --data-binary "@$UNITD_CONFIG" localhost:9000/config/)"
+    echo "$RESPONSE" | jq -e '.success' > /dev/null && {
         echo "Nextcloud started successfully"
         exit 0
     }
+    echo "$RESPONSE"
     echo "Failed to start nextcloud"
     exit 1
 }
