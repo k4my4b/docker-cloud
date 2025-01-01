11328785fb9a388d0ef03788906f4b7a3320e41f
Kamyab Sherafat <k4my4b@gmail.com>
2025-01-01 22:46:15 +0000

    feat: enhance nextcloud setup with (automatic) reverse-proxy configuration
    - Added `readConfigFile` and `writeConfigFile` functions in `autoconfig.php` to safely manage configuration files with locking and opcache invalidation.
    - Implemented automatic reverse-proxy settings in `autoconfig.php`:
      - Trusted proxies are now dynamically set based on the `HTTP_X_FORWARDED_FOR` or `HTTP_X_REAL_IP` headers, with fallback to `127.0.0.1`.
      - Merged trusted proxies are stored in `reverse-proxy.config.php`.
    - Adjusted file permissions in Dockerfile for `/etc/nextcloud` with `--chmod=660` to allow group write access.

--- nextcloud/etc/nextcloud/autoconfig.php
+++ nextcloud/etc/nextcloud/autoconfig.php
@@ -9,6 +9,52 @@ function getEnvValue($varName, $default = null) {
     return $value;
 }
 
+function readConfigFile($uri) {
+    // Invalidate opcache  if the timestamp changed
+    if (function_exists('opcache_invalidate')) {
+        @opcache_invalidate($uri, false);
+    }
+
+    $filePointer = file_exists($uri) ? @fopen($uri, 'r') : false;
+    if ($filePointer === false) {
+        throw new Exception(sprintf('FATAL: Could not open the config file %s', $uri));
+    }
+
+    if (!flock($filePointer, LOCK_SH)) {
+        throw new Exception(sprintf('Could not acquire a shared lock on the config file %s', $uri));
+    }
+
+    try {
+        include $uri;
+    } finally {
+        flock($filePointer, LOCK_UN);
+        fclose($filePointer);
+    }
+
+    return (isset($CONFIG) && is_array($CONFIG)) ? $CONFIG : false;
+}
+
+function writeConfigFile($uri, $data) {
+    $filePointer = file_exists($uri) ? @fopen($uri, 'w+') : false;
+    if ($filePointer === false) {
+        throw new Exception(sprintf('FATAL: Could not write to the config file %s', $uri));
+    }
+
+    if (!flock($filePointer, LOCK_EX)) {
+        throw new Exception(sprintf('Could not acquire an exclusive lock on the config file %s', $uri));
+    }
+
+    fwrite($filePointer, "<?php\n\$CONFIG = " . var_export($data, true) . ";\n");
+    fflush($filePointer);
+    flock($filePointer, LOCK_UN);
+    fclose($filePointer);
+
+    // Invalidate opcache regardless of timestamp
+    if (function_exists('opcache_invalidate')) {
+        @opcache_invalidate($uri, true);
+    }
+}
+
 // For now at least, only PostgreSQL is supported
 // TODO remove this warning when more databases are supported or ...
 $AUTOCONFIG = [
@@ -42,4 +88,30 @@ try {
 } catch(Exception $e) {
     error_log($e->getMessage());
     error_log('Skipping automatic admin account setup.');
+}
+
+// Reverse-proxy settings
+try {
+    $file = OC::$SERVERROOT.'/config/reverse-proxy.config.php';
+
+    $proxyConfig = [
+        'trusted_proxies' => getenv('TRUSTED_PROXIES') ?: (function() use ($file) {
+            // Multiple processes may run this code concurrently during installation.
+            // This means headers could contain different values at any given time.
+            // We append new values to the file rather than overwriting to preserve
+            // any existing configuration. We read and merge values from the file.
+
+            // Fallback to '127.0.0.1' if proxy headers are not set. This ensures the
+            // configuration always has a value, but beware in a distributed environment.
+            $trustedProxies = array_map('trim', explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['HTTP_X_REAL_IP'] ?? '127.0.0.1'));
+
+            // Merge with existing trusted proxies from the configuration file
+            return array_unique(array_merge(readConfigFile($file)['trusted_proxies'] ?? [], $trustedProxies));
+        })()
+    ];
+
+    writeConfigFile($file, $proxyConfig);
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    error_log('Skipping automatic reverse-proxy configuration.');
 }
---
+++ nextcloud/etc/nextcloud/reverse-proxy.config.php
@@ -0,0 +1,4 @@
+<?php
+// The contents of this file are automatically generated
+// by 'autoconfig.php' during installation. If you are seeing
+// this message, something went wrong during the configuration process.
